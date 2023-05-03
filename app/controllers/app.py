062119430b1e82387f5f14a41d6923f21b421071
nikhil-mahashabde <nikhil.mahashabde@gmail.com>
2023-05-03 21:54:34 +1000

    v1

--- app/controllers/app.py
+++ app/controllers/app.py
@@ -5,11 +5,10 @@ sys.path.insert(0, '..')
 from flask import Flask, render_template, session, redirect, url_for, request, g
 from datetime import timedelta
 import os
-import psycopg2
 from models.userAuthService import *
 
-app = Flask(__name__)
-
+app = Flask(__name__, template_folder='../templates')
+app.secret_key = 'laalaaland'
 app.config['SECRET_KEY'] = 'laalaaland'
 app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(seconds=20)
 
@@ -17,7 +16,13 @@ publicRoutes = ['/login', '/about', '/contact']
 
 @app.context_processor
 def templateData():
-    None
+    appData = dict(
+
+    session = session.get("user_id", ""),
+    userAuthId = UserAuthService().getUserAuthName(session),
+    userAuthData = UserAuthService().getUserAuthData(session) if session.get("user_id", "") else ""
+    )
+    return appData
 
 @app.before_request
 def launch():
